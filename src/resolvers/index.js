33e89fe856d43fb526570d0ee8e7e3704dd5d531
PickHD <taufikjanuar35@gmail.com>
2021-03-14 18:35:32 +0700

    Add resolver parser

---
+++ src/resolvers/index.js
@@ -0,0 +1,10 @@
+"use strict"
+const Mutation = require("./Mutation"),
+  Query = require("./Query"),
+  User = require("./User"),
+  Mk = require("./Mk"),
+  Secret = require("./Secret"),
+  Character = require("./Character"),
+  Move = require("./Move")
+
+module.exports = { Mutation, Query, User, Mk, Secret, Character, Move }
